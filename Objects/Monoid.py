881d53f3d1d4d9d2fa9241c99e2130adabf1c37a
levav <levavft@gmail.com>
2021-02-12 15:33:14 +0200

    create monoid object class

---
+++ Objects/Monoid.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from collections.abc import Callable, Mapping
+from typing import TypeVar
+
+T = TypeVar('T', int, float, str)
+
+
+class Monoid:
+    def __init__(self, one: T, elements: set[T], function: Callable[[T, T], T]):
+        """
+        We use the multiplicative notation, e.g: 1, S, f where:
+        for all a,b in S:
+        f(1,a)=f(a,1)=a
+        f(a,b) in S
+        :param one: 1
+        :param elements: S
+        :param function: f
+        """
+        assert one in elements
+        self.one = one
+        self.elements = elements
+        self.function = function
+
+    @staticmethod
+    def monoid_from_generators(generators: set[T], relation: Mapping[[T], T]) -> Monoid:
+        raise NotImplementedError
+
+    def submonoid_from_equivalence_relation(self, relation: Mapping[[T], T]) -> Monoid:
+        raise NotImplementedError
